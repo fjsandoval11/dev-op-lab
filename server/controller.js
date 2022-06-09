961df0334a41538dd12621aa611751f37d59529c
Freddy Sandoval <fjsandoval11@gmail.com>
2022-06-09 14:49:06 -0400

    devOp-lab

---
+++ server/controller.js
@@ -0,0 +1,51 @@
+const movies = require('./db.json')
+
+let globalId = 11
+
+
+
+module.exports = {
+    getMovies: (req, res)=>{
+        res.status(200).send(movies)
+    },
+     deleteMovie: (req, res) => {
+        let index = movies.findIndex(elem => elem.id === +req.params.id)
+        movies.splice(index, 1)
+        res.status(200).send(movies)
+    },
+    createMovie: (req, res) => {
+        let {title , rating, imageUrl} = req.body
+
+        let newMovie = {
+            id: globalId,
+            title,
+            rating: rating,
+            // when key and value have same name can just type name and it will be the same as ^
+            imageUrl: imageUrl
+        }
+     movies.push(newMovie)
+     globalId++
+     // ^^^^ equal to globalID = globalID + 1, think for loops
+     res.status(200).send(movies)
+    },
+    updateMovie: (req, res) => {
+        let id = req.params.id;
+        let type = req.body.type;
+
+        let index = movies.findIndex((elem) => +elem.id === +id);
+
+        if (movies[index].rating === 5 && type === "plus") {
+            res.status(400).send("cannot set a rating above 5");
+        } else if ((movies[index].rating === 0) & (type === "minus")) {
+            res.status(400).send("cannot set a rating below 0");
+        } else if (type === "plus") {
+            movies[index].rating++;
+            res.status(200).send(movies);
+        } else if (type === "minus") {
+            movies[index].rating--;
+            res.status(200).send(movies);
+        } else {
+            res.sendStatus(400);
+        }
+    },
+    }
