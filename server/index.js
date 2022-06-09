955dfe5f2b5d7689a51251de4265cdbc2f5a82f8
Freddy Sandoval <fjsandoval11@gmail.com>
2022-06-09 16:01:00 -0400

    fixed for hero

--- server/index.js
+++ server/index.js
@@ -1,33 +1,31 @@
 const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const app = express()
-
-app.use(express.static('public'))
+ const cors = require('cors')
+ const path = require('path')
+ const app = express()
 
 
 
 
-const controllerFile = require('./controller')
+
+ const controllerFile = require('./controller')
 
 
 
-app.use(express.json())
-app.use(cors())
+ app.use(express.json())
+ app.use(cors())
 
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+ app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, '/index.html'))
+ })
 
 
-app.get(`/api/movies`, controllerFile.getMovies)
-app.post(`/api/movies`, controllerFile.createMovie)
-app.delete(`/api/movies/:id`, controllerFile.deleteMovie)
-app.put(`/api/movies/:id`, controllerFile.updateMovie);
+ app.get(`/api/movies`, controllerFile.getMovies)
+ app.post(`/api/movies`, controllerFile.createMovie)
+ app.delete(`/api/movies/:id`, controllerFile.deleteMovie)
+ app.put(`/api/movies/:id`, controllerFile.updateMovie);
 
 
-const port = process.env.PORT || 4004
-app.listen(port, () => console.log(`running on 4004`))
-
+ const port = process.env.PORT || 4004
+ app.listen(port, () => console.log(`running on 4004`))
